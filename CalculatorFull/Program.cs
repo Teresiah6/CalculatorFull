8be8ce0b6cffead6a9dc092aa085568a44210191
Teresiah6 <wangariteresiah6@gmail.com>
2021-08-02 10:17:11 +0300

    Add project files.

---
+++ CalculatorFull/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CalculatorFull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+
+            Console.Write("Enter a number: ");
+            double num2= Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+
+            } else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+
+            }
+            else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+
+            }
+            else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
